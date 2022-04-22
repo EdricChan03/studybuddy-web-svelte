3db32e852e653d45e45b10e14dce424f7ad6d8c3
Edric <edric.chan.1997@gmail.com>
2022-04-23 00:25:46 +0800

    Set default font family

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,9 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    fontFamily: {
+      sans: ['"Baloo 2"', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [],
