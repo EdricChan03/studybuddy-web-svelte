799939ff1c0a1a97d81c1373b1ee110dbf58307c
Edric <edric.chan.1997@gmail.com>
2022-12-22 09:25:54 +0800

    NavItem: Add focus state

--- src/lib/components/nav/NavItem.svelte
+++ src/lib/components/nav/NavItem.svelte
@@ -11,14 +11,14 @@
   $: {
     if (!('separator' in link) && link.url !== undefined) {
       url = `/${link.url}`;
-      activeClasses = $page.url.pathname === url ? 'text-orange-700 bg-orange-200/50 hover:bg-orange-300/50 active:bg-orange-400/50'
-      : 'hover:bg-slate-200 active:bg-slate-300';
+      activeClasses = $page.url.pathname === url ? 'focus:ring-orange-500 text-orange-700 bg-orange-200/50 hover:bg-orange-300/50 active:bg-orange-400/50'
+      : 'focus:ring-slate-400 hover:bg-slate-200 active:bg-slate-300';
     }
   }
 </script>
 
 {#if !('separator' in link)}
-  <a href={url} class="flex items-center {activeClasses} rounded-[28px] h-14 p-4">
+  <a href={url} class="flex items-center transition-all focus:outline-none focus:ring {activeClasses} rounded-[28px] h-14 p-4">
     <span class="material-icons-outlined mr-3" aria-hidden="true">{link.icon}</span>
     <span>{link.name}</span>
   </a>
