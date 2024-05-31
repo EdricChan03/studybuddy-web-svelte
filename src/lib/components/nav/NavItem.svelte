e9a8a7d6d898a918b5d845b0d5a0015f9ebe26bb
Edric <edric.chan.1997@gmail.com>
2024-05-31 10:45:03 +0800

    refactor: use Lucide for icons
    * Add Lucide Svelte dependency
    * Remove Material Icons webfont import

--- src/lib/components/nav/NavItem.svelte
+++ src/lib/components/nav/NavItem.svelte
@@ -11,15 +11,20 @@
   $: {
     if (!('separator' in link) && link.url !== undefined) {
       url = `/${link.url}`;
-      activeClasses = $page.url.pathname === url ? 'focus:ring-orange-500 text-orange-700 bg-orange-200/50 hover:bg-orange-300/50 active:bg-orange-400/50'
-      : 'focus:ring-slate-400 hover:bg-slate-200 active:bg-slate-300';
+      activeClasses =
+        $page.url.pathname === url
+          ? 'focus:ring-orange-500 text-orange-700 bg-orange-200/50 hover:bg-orange-300/50 active:bg-orange-400/50'
+          : 'focus:ring-slate-400 hover:bg-slate-200 active:bg-slate-300';
     }
   }
 </script>
 
 {#if !('separator' in link)}
-  <a href={url} class="flex items-center transition-all focus:outline-none focus:ring {activeClasses} rounded-[28px] h-14 p-4">
-    <span class="material-icons-outlined mr-3" aria-hidden="true">{link.icon}</span>
+  <a
+    href={url}
+    class="flex items-center transition-all focus:outline-none focus:ring gap-3 {activeClasses} rounded-[28px] h-14 p-4"
+  >
+    <svelte:component this={link.icon} aria-hidden="true" />
     <span>{link.name}</span>
   </a>
 {:else}
--- src/lib/data/links.ts
+++ src/lib/data/links.ts
@@ -1,21 +1,31 @@
+import type { ComponentType } from 'svelte';
+
+import CalendarIcon from 'lucide-svelte/icons/calendar';
+import CircleCheckBigIcon from 'lucide-svelte/icons/circle-check-big';
+import CircleHelpIcon from 'lucide-svelte/icons/circle-help';
+import HomeIcon from 'lucide-svelte/icons/home';
+import InfoIcon from 'lucide-svelte/icons/info';
+import NewspaperIcon from 'lucide-svelte/icons/newspaper';
+import SettingsIcon from 'lucide-svelte/icons/settings';
+
 export type Link =
   | {
       name: string;
       url: string;
-      icon: string;
+      icon: ComponentType;
     }
   | { separator: true };
 
 export const links: Link[] = [
-  { name: 'Home', url: '', icon: 'home' },
-  { name: 'Tasks', url: 'tasks', icon: 'task_alt' },
-  { name: 'Calendar', url: 'calendar', icon: 'today' },
+  { name: 'Home', url: '', icon: HomeIcon },
+  { name: 'Tasks', url: 'tasks', icon: CircleCheckBigIcon },
+  { name: 'Calendar', url: 'calendar', icon: CalendarIcon },
   { separator: true },
-  { name: 'Help', url: 'help', icon: 'help_outline' },
+  { name: 'Help', url: 'help', icon: CircleHelpIcon },
   { separator: true },
-  { name: 'Settings', url: 'settings', icon: 'settings' },
-  { name: 'Release notes', url: 'release-notes', icon: 'new_releases' },
+  { name: 'Settings', url: 'settings', icon: SettingsIcon },
+  { name: 'Release notes', url: 'release-notes', icon: NewspaperIcon },
   { separator: true },
-  { name: 'Apps', url: 'apps', icon: 'apps' },
-  { name: 'About', url: 'about', icon: 'info' }
+  // { name: 'Apps', url: 'apps', icon: 'apps' },
+  { name: 'About', url: 'about', icon: InfoIcon }
 ];
